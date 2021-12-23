5b0bc3a74d837a27afdcd33ab328b927cf689936
daraphista <daraphistadaone@gmail.com>
2021-12-23 14:00:14 +0800

    finished hard bot prototype

--- script.js
+++ script.js
@@ -1,98 +1,180 @@
-const gameBoard = (() => {
-  const squareArray = ['', '', '', '', '', '', '', '', ''];
-  const squarePhysical = Array.from(document.querySelectorAll('.square'))
+const Gameboard = (() => {
+  const physicalBoard = [
+    Array.from(document.querySelectorAll('.row0')),
+    Array.from(document.querySelectorAll('.row1')),
+    Array.from(document.querySelectorAll('.row2')), 
+  ];
+  const getboardValues = () => {
+    const boardValues = [[],[],[]]
+    for(i = 0; i < 3; i++) {
+      for(j = 0; j < 3; j++) {
+        boardValues[i][j] = physicalBoard[i][j].firstChild.textContent;
+      }
+    }
+    return boardValues;
+  }
+  const boardValues = getboardValues();
   
-  const playGame = () => {
-    moves = 0;
-    window.addEventListener('click', () => {
-      _round(moves);
-      moves++;
-      displayResult();
-    })
+  const Robot = (symbol) => {
+    const easyBotMove = () => {
+      let row = Math.floor(Math.random() * 3);
+      let col = Math.floor(Math.random() * 3);
+
+      if(boardValues[row][col] == '') {
+        boardValues[row][col] = symbol;
+      } else {
+        easyBotMove();
+      }
+    };
+    const hardBotMove = () => {
+      let bestVal = -999;
+      let bestRow = -1;
+      let bestCol = -1;
+
+      for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+          if(boardValues[i][j] == '') {
+            boardValues[i][j] = symbol;
+            let moveVal = _minimax(boardValues, 0, false, this, botX);
+            boardValues[i][j] = '';
+            console.log(moveVal);
+
+            if(moveVal > bestVal) {
+              bestVal = moveVal;
+              bestRow = i;
+              bestCol = j;
+            }
+          }
+        }
+      }
+      
+      boardValues[bestRow][bestCol] = symbol;
+      console.log(bestCol, bestRow);
+    };
+    const _minimax = (boardValues, depth, isMax, player, opponent) => {
+
+      let score = evaluateWinner(botO.symbol, botX.symbol)
+
+      if(score == 10) {
+        return score;
+      }
+      if(score == -10) {
+        return score;
+      }
+      if(boardValues.every(row => row.every(square => square !== ''))) {
+        return 0;
+      }
+
+      if(isMax) {
+        let best = -999;
+
+        for(let i = 0; i < 3; i++) {
+          for(let j = 0; j < 3; j++) {
+            if(boardValues[i][j] == '') {
+              boardValues[i][j] = 'o';
+              best = Math.max(best, _minimax(boardValues, depth + 1, !isMax));
+              boardValues[i][j] = '';
+            }
+          }
+        }
+
+        return best;
+      } else {
+        let best = 999;
+
+        for(let i = 0; i < 3; i++) {
+          for(let j = 0; j < 3; j++) {
+            if(boardValues[i][j] == '') {
+              boardValues[i][j] = 'x';
+              best = Math.min(best, _minimax(boardValues, depth + 1, true));
+              boardValues[i][j] = '';
+            }
+          }
+        }
+
+        return best;
+      }
+    }
+
+    return { easyBotMove, hardBotMove, symbol };
+  }
+  const Player = (symbol) => {
+    const playerMove = () => {};
   }
 
-  const _round = (moves) => {
+  const updatePhysicalBoard = () => {
+    for(i = 0; i < 3; i++) {
+      for(j = 0; j < 3; j++) {
+        physicalBoard[i][j].firstChild.textContent = boardValues[i][j];
+      }
+    }
+  }
+  const evaluateWinner = (player, opponent) => {
+    for(let row = 0; row < 3; row++) {
+      if(boardValues[row][0] == boardValues[row][1] && boardValues[row][1] == boardValues[row][2]) {
+        if(boardValues[row][0] == player) {
+          return +10;
+        } else if(boardValues[row][0] == opponent) {
+          return -10;
+        }
+      }
+    }
+
+    for(let col = 0; col < 3; col++) {
+      if(boardValues[0][col] == boardValues[1][col] && boardValues[1][col] == boardValues[2][col]) {
+        if(boardValues[0][col] == player) {
+          return +10;
+        } else if(boardValues[0][col] == opponent) {
+          return -10;
+        }
+      }
+    }
+
+    if(boardValues[0][0] == boardValues[1][1] && boardValues[1][1] == boardValues[2][2]) {
+      if(boardValues[0][0] == player) {
+        return +10;
+      } else if(boardValues[0][0] == opponent) {
+        return -10;
+      }
+    }
+    if(boardValues[0][2] == boardValues[1][1] && boardValues[1][1] == boardValues[2][0]) {
+      if(boardValues[0][2] == player) {
+        return +10;
+      } else if(boardValues[2][0] == opponent) {
+        return -10;
+      }
+    }
+
+    return 0;
+  }
+  const displayResults = (player, opponent) => {
+    if(evaluateWinner(botX.symbol, botO.symbol) == 10) {
+      alert(`omg nanalo si X`);
+      location.reload();
+    } else if(evaluateWinner(botX.symbol, botO.symbol) == -10) {
+      alert(`omg nanalo si O`);
+      location.reload();
+    } else if(boardValues.every(row => row.every(square => square !== ''))) {
+      alert(`omg it's a draw`);
+      location.reload();
+    }
+  }
+
+  const botX = Robot('x');
+  const botO = Robot('o');
+
+  let moves = 0
+
+  window.addEventListener('click', () => {
     if(moves % 2 == 0) {
-      _compMove(squareArray, 'x');
+      botX.easyBotMove();
     } else {
-      _compMove(squareArray, 'o');
+      // botO.easyBotMove();
+      botO.hardBotMove();
     }
-    _displayMoves(squarePhysical, squareArray);
-  }
-  
-  const _displayMoves = (squarePhysical, squareArray) => {
-    for(i = 0; i < 9; i++) {
-      squarePhysical[i].firstChild.textContent = squareArray[i];
-    }
-  }
-  
-  const _compMove = (squares, symbol) => {
-    let index = Math.floor(Math.random() * 10)
-    if (squares[index] === '') {
-      squares[index] = symbol;
-    } else {
-      _compMove(squares, symbol);
-    }
-  }
-  
-  const allChecked = (winConditions, symbol) => {
-    return winConditions.every(
-      function(winConditions) {
-        return squarePhysical[winConditions].firstChild.textContent === symbol;
-      }
-      )
-  }  
-    
-  const _isDraw = (squares) => {
-    return squares.every(
-      function(square) {
-        return square !== ''
-      }
-      )
-  }
-    
-  const displayResult = () => {
-    console.log('lmao');
-    if(playerX.isWinner()) {
-      setTimeout(function() {
-        alert('X wins!');
-        location.reload();
-      }, 20);
-    } else if(playerO.isWinner()) {
-      setTimeout(function() {
-        alert('O wins!');
-        location.reload();
-      }, 20);
-    } else if(_isDraw(squareArray)) {
-      setTimeout(function() {
-        alert('It\'s a draw!');
-        location.reload();
-      }, 20);
-    }
-  }
+    moves++;
+    setTimeout(() => {displayResults(botX, botO)}, 20)
+    updatePhysicalBoard();
+  })
 
-  return { playGame, allChecked, displayResult };
 })();
-
-const Player = (symbol, isBot) => {
-  const isWinner = () => {
-    if(gameBoard.allChecked([0, 1, 2], symbol) 
-    || gameBoard.allChecked([3, 4, 5], symbol) 
-    || gameBoard.allChecked([6, 7, 8], symbol) 
-    || gameBoard.allChecked([0, 3, 6], symbol) 
-    || gameBoard.allChecked([1, 4, 7], symbol) 
-    || gameBoard.allChecked([2, 5, 8], symbol)
-    || gameBoard.allChecked([0, 4, 8], symbol)
-    || gameBoard.allChecked([2, 4, 6], symbol)) {
-      return true
-    } else {
-      return false
-    }
-  }
-  return { symbol, isBot, isWinner };
-}
-
-const playerX = Player('x', true);
-const playerO = Player('o', true);
-
-gameBoard.playGame();
