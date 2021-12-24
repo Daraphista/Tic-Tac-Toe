7dfcbd22dc4144cb09d79acbaf3fc1f67ff713bf
daraphista <daraphistadaone@gmail.com>
2021-12-24 18:49:59 +0800

    added player selection

--- script.js
+++ script.js
@@ -14,7 +14,96 @@ const Gameboard = (() => {
     return boardValues;
   }
   const boardValues = getboardValues();
+  const updatePhysicalBoard = () => {
+    for(i = 0; i < 3; i++) {
+      for(j = 0; j < 3; j++) {
+        physicalBoard[i][j].firstChild.textContent = boardValues[i][j];
+        if(boardValues[i][j] != '') {
+          physicalBoard[i][j].style.pointerEvents = 'none';
+        }
+      }
+    }
+  }
+  const evaluateWinner = (playerSymbol, opponentSymbol) => {
+    for(let row = 0; row < 3; row++) {
+      if(boardValues[row][0] == boardValues[row][1] && boardValues[row][1] == boardValues[row][2]) {
+        if(boardValues[row][0] == playerSymbol) {
+          return +10;
+        } else if(boardValues[row][0] == opponentSymbol) {
+          return -10;
+        }
+      }
+    }
 
+    for(let col = 0; col < 3; col++) {
+      if(boardValues[0][col] == boardValues[1][col] && boardValues[1][col] == boardValues[2][col]) {
+        if(boardValues[0][col] == playerSymbol) {
+          return +10;
+        } else if(boardValues[0][col] == opponentSymbol) {
+          return -10;
+        }
+      }
+    }
+
+    if(boardValues[0][0] == boardValues[1][1] && boardValues[1][1] == boardValues[2][2]) {
+      if(boardValues[0][0] == playerSymbol) {
+        return +10;
+      } else if(boardValues[0][0] == opponentSymbol) {
+        return -10;
+      }
+    }
+    if(boardValues[0][2] == boardValues[1][1] && boardValues[1][1] == boardValues[2][0]) {
+      if(boardValues[0][2] == playerSymbol) {
+        return +10;
+      } else if(boardValues[2][0] == opponentSymbol) {
+        return -10;
+      }
+    }
+
+    return 0;
+  }
+  const displayResults = (playerSymbol, opponentSymbol) => {
+    if(evaluateWinner(playerSymbol, opponentSymbol) == 10) {
+      alert(`omg nanalo si ${playerSymbol}`);
+      location.reload();
+    } else if(evaluateWinner(playerSymbol, opponentSymbol) == -10) {
+      alert(`omg nanalo si ${opponentSymbol}`);
+      location.reload();
+    } else if(boardValues.every(row => row.every(square => square !== ''))) {
+      alert(`omg it's a draw`);
+      location.reload();
+    }
+  }
+
+  let selectedOption;
+  let selection = 'Player';
+  const dropUps = Array.from(document.querySelectorAll('.dropup'));
+  window.addEventListener('click', (e) => {
+    const selectionMenus = Array.from(document.querySelectorAll('.player'));
+    selectionMenus.forEach(selectionMenu => {
+      if(e.target.parentNode == selectionMenu || e.target.parentNode.parentNode == selectionMenu) {
+        selectedOption = selectionMenu.firstElementChild;
+        selectionMenu.classList.toggle('active');
+      } else {
+        selectionMenu.classList.remove('active');
+      }
+      dropUps.forEach(dropUp => {
+        if(e.target.parentNode == dropUp) {
+          selection = e.target.textContent;
+        }
+      })
+    })
+  })
+  window.addEventListener('mouseover', (e) => {
+    dropUps.forEach(dropUp => {
+      if(e.target.parentNode == dropUp) {
+        selectedOption.firstElementChild.textContent = e.target.textContent;    
+      } else {
+        selectedOption.firstElementChild.textContent = selection;
+      }
+    })
+  })
+  
   const Robot = (symbol) => {
     const easyBotMove = () => {
       let row = Math.floor(Math.random() * 3);
@@ -116,94 +205,19 @@ const Gameboard = (() => {
     return { playerMove };
   }
 
-  const updatePhysicalBoard = () => {
-    for(i = 0; i < 3; i++) {
-      for(j = 0; j < 3; j++) {
-        physicalBoard[i][j].firstChild.textContent = boardValues[i][j];
-        if(boardValues[i][j] != '') {
-          physicalBoard[i][j].style.pointerEvents = 'none';
-        }
-      }
-    }
-  }
-  const evaluateWinner = (playerSymbol, opponentSymbol) => {
-    for(let row = 0; row < 3; row++) {
-      if(boardValues[row][0] == boardValues[row][1] && boardValues[row][1] == boardValues[row][2]) {
-        if(boardValues[row][0] == playerSymbol) {
-          return +10;
-        } else if(boardValues[row][0] == opponentSymbol) {
-          return -10;
-        }
-      }
+  const playerX = document.querySelector('.player.x .selected');
+  const playerO = document.querySelector('.player.o .selected');
+
+  window.addEventListener('click', () => {
+    if(playerX.textContent.indexOf('Player') !== -1 && playerO.textContent.indexOf('Player') !== -1) {
+      console.log('PvP');
+    } else if(playerX.textContent.indexOf('Player') !== -1 && playerO.textContent.indexOf('Bot') !== -1) {
+      console.log('PvB');
+    } else if(playerX.textContent.indexOf('Bot') !== -1 && playerO.textContent.indexOf('Player') !== -1) {
+      console.log('BvP');
+    } else if (playerX.textContent.indexOf('Bot') !== -1 && playerO.textContent.indexOf('Bot') !== -1) {
+      console.log('BvB');
     }
 
-    for(let col = 0; col < 3; col++) {
-      if(boardValues[0][col] == boardValues[1][col] && boardValues[1][col] == boardValues[2][col]) {
-        if(boardValues[0][col] == playerSymbol) {
-          return +10;
-        } else if(boardValues[0][col] == opponentSymbol) {
-          return -10;
-        }
-      }
-    }
-
-    if(boardValues[0][0] == boardValues[1][1] && boardValues[1][1] == boardValues[2][2]) {
-      if(boardValues[0][0] == playerSymbol) {
-        return +10;
-      } else if(boardValues[0][0] == opponentSymbol) {
-        return -10;
-      }
-    }
-    if(boardValues[0][2] == boardValues[1][1] && boardValues[1][1] == boardValues[2][0]) {
-      if(boardValues[0][2] == playerSymbol) {
-        return +10;
-      } else if(boardValues[2][0] == opponentSymbol) {
-        return -10;
-      }
-    }
-
-    return 0;
-  }
-  const displayResults = (playerSymbol, opponentSymbol) => {
-    if(evaluateWinner(playerSymbol, opponentSymbol) == 10) {
-      alert(`omg nanalo si ${playerSymbol}`);
-      location.reload();
-    } else if(evaluateWinner(playerSymbol, opponentSymbol) == -10) {
-      alert(`omg nanalo si ${opponentSymbol}`);
-      location.reload();
-    } else if(boardValues.every(row => row.every(square => square !== ''))) {
-      alert(`omg it's a draw`);
-      location.reload();
-    }
-  }
-
-  const botX = Robot('x');
-  const botO = Robot('o');
-  const playerX = Player('x');
-  const playerO = Player('o');
-
-  let moves = 0;
-  window.addEventListener('click', (e) => {
-    if(moves % 2 == 0) {
-      playerX.playerMove(e.target);
-    } else {
-      playerO.playerMove(e.target);
-    }
-    setTimeout(() => {displayResults(botX.symbol, botO.symbol)}, 20)
-    if(e.target.classList.contains('square')) {moves++;}
   })
-
-  // window.addEventListener('click', () => {
-  //   if(moves % 2 == 0) {
-  //     // botX.easyBotMove();
-  //     botX.hardBotMove();
-  //   } else {
-  //     // botO.easyBotMove();
-  //     botO.hardBotMove();
-  //   }
-  //   moves++;
-  //   setTimeout(() => {displayResults(botX.symbol, botO.symbol)}, 20)
-  //   updatePhysicalBoard();
-  // })
-
 })();
